1204dc3757b580d9a5b11bee6c3bde90739589d0
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-11-15 20:23:39 -0800

    dispatching an action event

---
+++ redux/dispatch-an-action-event.js
@@ -0,0 +1,18 @@
+//dispatch method is what you use to dispatch actions to the Redux store. 
+//Calling store.dispatch() and passing the value returned from an action creator sends an action back to the store:
+
+//store.dispatch(actionCreator()); or
+//store.dispatch({ type: 'LOGIN' });
+
+const store = Redux.createStore(
+  (state = { login: false }) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Dispatch the action here:
+store.dispatch(loginAction())
