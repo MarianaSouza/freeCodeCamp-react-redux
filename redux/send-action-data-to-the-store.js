8148f06e495cb0d8d7236719e8b23397e483b4af
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-11-17 23:38:15 -0800

    sending specific data along with actions

---
+++ redux/send-action-data-to-the-store.js
@@ -0,0 +1,26 @@
+//To send specific data along with your actions:
+
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+     case ADD_NOTE:
+     return action.text
+    default:
+      return state;
+  }
+};
+
+//action
+const addNoteText = (note) => {
+ return {
+   type: ADD_NOTE,
+   text: note
+ }
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState()); // Initial State
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState()); //Hello
