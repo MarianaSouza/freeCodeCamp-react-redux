5e6629769440bd04396a37a50f7b7092d69b69ba
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-11-21 21:39:53 -0800

    creating a to-do list without mutating the state

---
+++ redux/never-mutate-state.js
@@ -0,0 +1,32 @@
+// Immutable state means that you never modify state directly, instead, you return a new copy of state.
+
+const ADD_TO_DO = 'ADD_TO_DO';
+
+// A list of strings representing tasks to do:
+
+const todos = [
+  'Go to the store',
+  'Clean the house',
+  'Cook dinner',
+  'Learn to code',
+];
+
+const addToDo = (todo) => {
+  return {
+    type: ADD_TO_DO,
+    todo
+  }
+}
+
+const immutableReducer = (state = todos, action) => {
+  switch (action.type) {
+    case ADD_TO_DO:
+      // Don't mutate state:
+      return [...state].concat(action.todo);
+    default:
+      return state;
+  }
+};
+
+
+const store = Redux.createStore(immutableReducer);
